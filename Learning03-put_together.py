3d4101e23fc0acc95a72ed5ebac768b9c106ec8a
Silver Geek Sea <SilverGeekSea@outlook.com>
2020-12-18 20:39:07 +1100

    try to cross

---
+++ Learning03-put_together.py
@@ -0,0 +1,15 @@
+""" 
+    Put what we learned
+    so far together. 
+"""
+
+from urllib.request import urlopen
+story = urlopen('http://sixty-north.com/c/t.txt')
+story_words = []
+for line in story:
+    line_words = line.decode('utf8').split()
+    for word in line_words:
+        story_words.append(word)
+
+story.close()
+print (f'storty_words is {story_words}')
